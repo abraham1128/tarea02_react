24b02a682318848c8c21a6459c073a8d69afe372
ABRAHAM-PC\Abraham <abraham1128@gmail.com>
2021-06-05 23:30:24 -0400

    cambiada ubicacion de menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import imageHeader from './Servicios.jpg'
-import './Header.module.css'
+import imageHeader from './Servicios.jpg';
+import './Header.module.css';
 
-function Header() {
-
+export default function Header() {
     return (
         <header className="py-5 bg-image-full">
             <div className="text-center my-5">
                 <img className="img-fluid rounded-circle mb-4" alt="..."  src={imageHeader}  />
                 <h1 className="text-white fs-3 fw-bolder">Servicios</h1>
             </div>
-        </header >
+        </header>
     );
 };
-
-export default Header
